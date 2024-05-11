23d423c91d377c3aea9a46f4070a2fee1a1f5f01
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-05-11 10:47:16 +0700

    set up for spring boot + jsp

---
+++ src/main/java/com/example/java5_s2_controllers/Java5S2ControllersApplication.java
@@ -0,0 +1,14 @@
+package com.example.java5_s2_controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5S2ControllersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java5S2ControllersApplication.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/com/example/java5_s2_controllers/Java5S2ControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java5_s2_controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java5S2ControllersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
